12ef00f2af8fc44d3d7e7e4af2614cae70843aad
sarthib7 <sarthiborkar7@gmail.com>
2026-05-26 17:02:55 +0200

    Log GitHub sync runner to stdout

--- scripts/run_github_sync.py
+++ scripts/run_github_sync.py
@@ -19,6 +19,7 @@ from urllib.request import Request, urlopen
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    stream=sys.stdout,
 )
 logger = logging.getLogger(__name__)
 
